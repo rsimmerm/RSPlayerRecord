501b986a37afa26c22fa12dba9272eec44f028af
Ross Simmerman <rosssimmerman@Rosss-MacBook-Air.local>
2014-07-01 16:39:55 -0400

    added part2

---
+++ RSPlayerRecord/DetailedViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailedViewController.h
+//  RSPlayerRecord
+//
+//  Created by Ross Simmerman on 7/1/14.
+//  Copyright (c) 2014 RossSimmerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailedViewController : UIViewController
+
+@end
